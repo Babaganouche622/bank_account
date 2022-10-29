851382da9550a71804fb687bcf69a0fdf583c5f0
Brian <briangordon.cahill@students.dominican.edu>
2022-10-29 14:28:51 -0700

    M3- Added the bank class, organised the account class

--- account.py
+++ account.py
@@ -1,14 +1,27 @@
 import random
 import re
+import time
 
 
 class Account:
-    def __init__(self, name, password, balance):
-        self.name = name
+    def __init__(self, first_name, last_name, password, balance):
+        self.first_name = first_name
+        self.last_name = last_name
         self.password = password
         self.balance = balance
         self.number = ''
 
+
+
+    def get_balance(self):
+        return self.balance
+
+    def set_account_name(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_account_name(self):
+        return f"{self.first_name} {self.last_name}"
 
     def set_account_number(self):
         number = []
@@ -16,47 +29,38 @@ class Account:
             return print("Account number is already set.")
         while len(number) < 8:
             number.append(random.randint(1, 9))
-
         number = int(''.join(map(str,number)))
         self.number = number
-
-
-    def get_balance(self):
-        return self.balance
-
-    def get_account_name(self):
-        return self.name
 
     def get_account_number(self):
         number = re.sub("(\d)", "*", str(self.number), 4)
         return number
 
+    def set_account_password():
+        pass
+
     def get_account_password(self):
         return self.password
 
+    def set_deposit(self, amount):
+        self.balance += round(float(amount), 2)
+        print(f"You deposited {amount}. Your balance is {self.balance}$")
+        time.sleep(1)
 
-    def set_account_balance():
-        """
-        Ask the user if they want to withdraw or Deposit before coming inside the class
-        here we will check which the user input, then condition which Withdraw/Deposit will trigger
-        Passthrough some number, validate that it is a number and rounded to the 0.00$ correct decimal.
-        If the user is depositing the number should be positive.
-        If the user is withdrawing the number should be nugative AND less than the total account balance
-        If the withdraw amount is over the account total, kick out to main menu and charge a 10$NSF
-
-        Else set the amount to be deposited/withdrawn in the account. 
-        """
-        pass
+    def set_withdraw(self, amount):
+        if self.balance < amount:
+            self.balance -= 10
+            raise print("Ho ho ho ho, you didn't check your balance first. Thanks bucko, CHA CHING!")
+        else:
+            self.balance -= round(float(amount), 2)
+            print(f"You withdrew {amount}. Your balance is {self.balance}$")
+            time.sleep(1)    
     
-    
-    def set_account_password():
-        pass
-    def set_account_name():
-        pass
 
 
-brian = Account('Brian', "Password", 0)
+brian = Account("Brian", "Cahill", "Password", 0)
 brian.set_account_number()
 print(brian.get_account_number())
 print(brian.__dict__)
 brian.set_account_number()
+brian.set_deposit(50000)
--- bank.py
+++ bank.py
@@ -1,4 +1,43 @@
-import account 
+from account import Account
+import time
+
+class Bank:
+    def __init__(self):
+        self.accounts = [
+            Account("Brian", "Cahill", "Big-Daddy69", 47000042),
+            Account("Josh", "Faigan", "Charmander4Life", 55555555),
+            Account("Andrew", "Aisling", "Vu/n3r@b/3-S3cur1\y", 69696969),
+            Account("Alex", "Rocha", "PwningN00bs", 81680085)
+        ]
+        
+    def set_new_account(self, first_name, last_name, password, balance):
+        self.accounts.append(Account(first_name, last_name, password, balance))
+
+    def transfer(self, account_number_from, account_number_to, amount):
+        for account in self.accounts:
+            if account.get_account_number() == account_number_from:
+                account.set_withdraw(amount)
+        for account in self.accounts:
+            if account.get_account_number() == account_number_to:
+                account.set_deposit(amount)
+
+    def banking():
+        is_banking = True
+        while is_banking == True:
+            choice = input("Welcome to the criminal bank of Money. Would you like to give us money today?\n1: Yes\n2: No\nSelection: ")
+            match choice:
+                case "1":
+                    print("Excellent!")
+                    time.sleep(1)
+                    input("Do you have an account with us already?\n1: Yes\n2: No\nEnter selection: ")
+                    
+                case "2":
+                    print("Too bad, we'll take your money another time!")
+                case _:
+                    print(f"{choice} not compatible, enter correct choice.")
+                    time.sleep(1)
+
+
 # 
 
 # build Banking class here
@@ -11,6 +50,15 @@ import account
 # Account name and password
 # type of account creating (Chequing/Savings)
 # How much money they want to deposit
+
+def creat_account():
+    pass
+
+def transfer():
+    pass
+
+def statement():
+    pass
 
 
 
